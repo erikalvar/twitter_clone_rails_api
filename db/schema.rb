546a77f26f30dde1c587f03f8aab9928f8c5a35d
Erik Alvar <erikalvarbass@gmail.com>
2022-01-21 16:08:00 -0500

    adds tweet resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_175005) do
+ActiveRecord::Schema.define(version: 2022_01_21_210634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
